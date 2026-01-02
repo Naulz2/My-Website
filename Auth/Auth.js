a42b67ae5d553f8ffffa1b0c53851afef1e10b96
Naul <naulgnz2@gmail.com>
2026-01-02 10:09:33 +0700

    Refactor authentication functions for user registration

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -10,23 +10,52 @@ signInBtn.addEventListener('click', () => {
     container.classList.remove('right-panel-active');
 });
 
-// Nếu đã đăng nhập rồi → chuyển trang
-if (localStorage.getItem("user")) {
-    window.location.href = "../music.html";
+function getUsers() {
+  return JSON.parse(localStorage.getItem("users")) || [];
+}
+
+function saveUsers(users) {
+  localStorage.setItem("users", JSON.stringify(users));
+}
+localStorage.removeItem("users");
+id=""
+function register() {
+  let user = regUser.value.trim();
+  let pass = regPass.value.trim();
+
+  if (!user || !pass) {
+    alert("Vui lòng nhập đủ thông tin");
+    return;
+  }
+
+  let users = getUsers();
+
+  if (users.find(u => u.username === user)) {
+    alert("Tài khoản đã tồn tại");
+    return;
+  }
+
+  users.push({
+    username: user,
+    password: pass
+  });
+
+  saveUsers(users);
+  alert("Đăng ký thành công!");
+  showLogin();
 }
 
 function login() {
-    let user = document.getElementById("username").value;
-    let pass = document.getElementById("password").value;
+  let user = loginUser.value.trim();
+  let pass = loginPass.value.trim();
 
-    if (user === "luan@gmail.com" && pass === "123") {
+  let users = getUsers();
+  let found = users.find(u => u.username === user && u.password === pass);
 
-        // Lưu thông tin đăng nhập
-        localStorage.setItem("user", user);
-
-        alert("Đăng nhập thành công!");
-        window.location.href = "../music.html";
-    } else {
-        alert("Sai email hoặc mật khẩu!");
-    }
-}
+  if (found) {
+    alert("Đăng nhập thành công!");
+    window.location.href
+  } else {
+    alert("Sai tài khoản hoặc mật khẩu");
+  }
+      }
