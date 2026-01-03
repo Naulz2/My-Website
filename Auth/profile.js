a6b03b70b3768286917c801227827e5b624ef23c
Naul <naulgnz2@gmail.com>
2026-01-03 17:53:58 +0700

    Add files via upload

---
+++ Auth/profile.js
@@ -0,0 +1,37 @@
+const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+if (!currentUser) {
+  alert("Vui lòng đăng nhập!");
+  window.location.href = "Auth.html";
+}
+
+// Hiển thị thông tin
+document.getElementById("username").textContent = currentUser.username;
+document.getElementById("pDate").textContent =
+  currentUser.data?.createdAt || "Không rõ";
+
+//Trang chủ
+function goHome() {
+  window.location.href = "../music.html";
+}
+
+// Đăng xuất
+function logout() {
+  localStorage.removeItem("currentUser");
+  window.location.href = "Auth.html";
+}
+
+// Xóa tài khoản
+function deleteAccount() {
+  if (!confirm("Bạn có chắc muốn xóa tài khoản không?")) return;
+
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+
+  users = users.filter(u => u.username !== currentUser.username);
+
+  localStorage.setItem("users", JSON.stringify(users));
+  localStorage.removeItem("currentUser");
+
+  alert("Tài khoản đã bị xóa");
+  window.location.href = "Auth.html";
+}
